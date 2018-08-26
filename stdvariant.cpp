d93ed286a8b3157f13d0ed84cc6b3c7932b1db5b
simplifiedinfive <42615618+simplifiedinfive@users.noreply.github.com>
2018-08-26 16:20:39 +0530

    replace endl with \n

--- stdvariant.cpp
+++ stdvariant.cpp
@@ -8,16 +8,16 @@ class student
 	int rollno;
 	float percentage;
 public:
-	student() { std::cout << "In student constructor" << std::endl; };
-	~student() { std::cout << "In student destructor" << std::endl; }
+	student() { std::cout << "In student constructor\n"; };
+	~student() { std::cout << "In student destructor\n"; }
 };
 class teacher
 {
 	std::string name;
 	std::vector<std::string> subjects;
 public:
-	teacher() { std::cout << "In teacher constructor" << std::endl; };
-	~teacher(){ std::cout << "In teacher destructor" << std::endl; }
+	teacher() { std::cout << "In teacher constructor\n"; };
+	~teacher(){ std::cout << "In teacher destructor\n"; }
 };
 class myClass
 {
@@ -29,18 +29,18 @@ public:
 int main()
 {
 	std::variant<int, float> defaultvar;
-	std::cout << std::get<int>(defaultvar) << std::endl;	//prints 0
+	std::cout << std::get<int>(defaultvar) << "\n";	//prints 0
 	
 	//std::variant<myClass, int, float> vartry;	//Doesn't compile because no default constructor for myClass
 	std::variant<std::monostate,myClass, int, float> vartry;	//works!
 	if (std::holds_alternative<int>(vartry) || std::holds_alternative<float>(vartry) || std::holds_alternative<myClass>(vartry))
 	{
-		std::cout << "vartry default initialized" << std::endl;
+		std::cout << "vartry default initialized" << "\n";
 	}
 	else
 	{
 		auto x = std::get<std::monostate>(vartry);
-		std::cout << typeid(x).name() << std::endl;
+		std::cout << typeid(x).name() << "\n";
 	}
 	
 	std::variant<student,int, float> var1;
@@ -60,7 +60,7 @@ int main()
 	}
 	catch (const std::exception &ex)
 	{
-		std::cout << typeid(ex).name() << " " << ex.what()<<std::endl;
+		std::cout << typeid(ex).name() << " " << ex.what()<<"\n";
 	}
 
 	try {
@@ -68,15 +68,15 @@ int main()
 	}
 	catch (const std::exception &ex)
 	{
-		std::cout << typeid(ex).name() << " " << ex.what()<<std::endl;
+		std::cout << typeid(ex).name() << " " << ex.what()<<"\n";
 	}
 
 	int *i = std::get_if<int>(&var1);
 	if(i)
-		std::cout << *i << std::endl;
+		std::cout << *i << "\n";
 	float *f3 = std::get_if<float>(&var1);
 	if (f3)
-		std::cout << *f3 << std::endl;
+		std::cout << *f3 << "\n";
 	
 	if (std::holds_alternative<float>(var1) == true) // succeeds
 	{
@@ -93,5 +93,24 @@ int main()
 	}
 	std::variant<int, float> var2 = 20;
 	std::visit([](auto const &e) {std::cout << e; }, var2);
+	
+	struct mutltiVisitor
+	{
+		void operator() (std::string &s)
+		{
+			std::cout << s << "\t" << s;
+		}
+		void operator ()(int i)
+		{
+			std::cout << i*i;
+		}
+		void operator ()(float f)
+		{
+			std::cout << f * f;
+		}
+	};
+
+	std::variant < std::string, int, float> var3 = 24;
+	std::visit(mutltiVisitor(), var3);
 	return 0;
 }
